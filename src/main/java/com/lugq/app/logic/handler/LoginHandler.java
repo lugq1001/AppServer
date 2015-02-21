6718739b73c8ade10ebdcb2b5bf8a60ea8e577a1
Lu Guangqing <lugq1001@icloud.com>
2015-02-21 19:34:44 +0800

    sync

--- src/main/java/com/lugq/app/logic/handler/LoginHandler.java
+++ src/main/java/com/lugq/app/logic/handler/LoginHandler.java
@@ -25,11 +25,11 @@ public class LoginHandler extends AppLogicHandler {
 			String reqData = message.getReq_data();
 			logger.debug(reqData);
 			LoginRequest req = objMapper.readValue(reqData, LoginRequest.class);
-			
+
 			String username = req.username;
 			String password = req.password;
 			String s = req.getS();
-			
+
 			// 验证MD5
 			String magicKey = AppConfig.getInstance().getNetworkConfig().getMagicKey();
 			String verify = MD5Util.md5(username + magicKey + password);
@@ -38,35 +38,35 @@ public class LoginHandler extends AppLogicHandler {
 				sendFailureResp(message, LoginResult.FailureVerify);
 				return;
 			}
-			
+
 			// 用户名为空
 			if (LangUtil.isEmpty(username)) {
 				logger.debug("-登录失败-用户名为空");
 				sendFailureResp(message, LoginResult.FailureUserNone);
 				return;
 			}
-			
+
 			// 查找用户
 			User u = User.findByUsername(username);
-			
+
 			// 用户不存在
 			if (u == null) {
 				logger.debug("-登录失败-" + username + " 不存在");
 				sendFailureResp(message, LoginResult.FailureUserNone);
 				return;
 			}
-			
+
 			// 密码错误
 			if (!u.getPassword().equals(password)) {
 				logger.debug("-登录失败-" + username + " 密码错误");
 				sendFailureResp(message, LoginResult.FailurePasswordError);
 				return;
 			}
-			
+
 			// 更新用户最后登录时间
 			u.setLateseLogin(System.currentTimeMillis());
 			u.save();
-			
+
 			// 响应
 			LoginResult result = LoginResult.Success;
 			LoginResponse resp = new LoginResponse(result.ordinal(), AppMessage.get(result.i18nCode));
@@ -81,29 +81,29 @@ public class LoginHandler extends AppLogicHandler {
 	}
 
 	private static class LoginRequest extends BaseRequest {
-		
+
 		public String username;
 		public String password;
-	
+
 	}
-	
+
 	public class LoginResponse extends BaseResponse {
 
 		public LoginResponse(int retCode, String retMsg) {
 			super(retCode, retMsg);
 			reqid = MessageID.USR_LOGIN;
 		}
-		
+
 		public User user;
 	}
-	
+
 	private void sendFailureResp(SBMessage message, LoginResult result) {
 		LoginResponse resp = new LoginResponse(result.ordinal(), AppMessage.get(result.i18nCode));
 		message.send(resp);
 	}
 
 	private enum LoginResult {
-		Success(MessageID.USR_LOGIN + ".succ"), 
+		Success(MessageID.USR_LOGIN + ".succ"),
 		Failure(MessageID.USR_LOGIN + ".failure"),
 		FailureVerify(MessageID.USR_LOGIN + ".failure.verify"),
 		FailureUserNone(MessageID.USR_LOGIN + ".failure.user.none"),
@@ -116,6 +116,6 @@ public class LoginHandler extends AppLogicHandler {
 		}
 	}
 
-	
+
 
 }
